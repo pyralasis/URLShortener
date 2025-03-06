6c9e6a4590b1748ef421dba8be6d8cb2fab6fc46
pyralasis <pyralas29@gmail.com>
2025-03-06 11:50:08 -0500

    update

--- src/Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
+++ src/Backend/obj/Debug/net9.0/Backend.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5549b5b5f9cd0abbdf7e9d09e486604ae0d41608")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- src/Frontend/src/App.tsx
+++ src/Frontend/src/App.tsx
@@ -1,5 +1,6 @@
+import { SetStateAction, useState } from 'react';
 import './App.css'
-import { useParams } from 'react-router'
+import { useNavigate, useParams } from 'react-router'
 
 function App() {
   let a = async (url: string) => {
@@ -17,6 +18,23 @@ function App() {
     a(url).then(b => {window.location.replace(new URL(b))});
   }
   // a();
+
+  const [inputValue, setInputValue] = useState('');
+  const handleInputChange = (event: { target: { value: SetStateAction<string>; }; }) => {
+    setInputValue(event.target.value);
+  };
+  
+  let submitURL = async () => {
+    let response = await fetch('http://backend:8080/shortener/shortenurl/',
+      {
+        method: "POST",
+        body: JSON.stringify({"url": inputValue})
+      }
+    );
+    console.log(response)
+    let navigate = useNavigate();
+    navigate("/info/")
+  };
   
   return (
     <>
@@ -24,8 +42,12 @@ function App() {
         <h1>
           Shortener
         </h1>
-        <input type="text" />
-        <button>Shorten</button>
+        <input 
+          type="text" 
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <button onClick={submitURL}>Shorten</button>
         {/* <img src="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAApQAAAKUAQAAAACStJWRAAAB40lEQVR4nO3aUW7CMAwA0N5g978lN2ASlDmxQwSknbTp+aMiifPCnxW32/XwuGxMJpPJZDKZTCaTyWQymUwmk8lkMpnM/2BuOb6atPswFqbJTCaTyWSumDGfhjdz2+7JuzlJZjKZTCZz0Wyr2GzrKJnJZDKZzJPMJLV5TCaTyWT+mnkbjHuATCaTyWSeZ46OSKsFSXuZTCaTyVwzU0QpGz1qMpPJZDKZB5hPY99wKdVuHEwmk8lkrpndrtIDfNy+9rzoC6YjmEwmk8lcM7sNMUxwILHAZDKZTOZR5rzL1x6RVic1jslkMpnMd80djrR6tSrlrfvFZDKZTOYhZly3UoMvrlupJVjvYUwmk8lkrpl7Wtfba3Pj2NgRtbBNYTKZTCbzczO4tp492f9BD5DJZDKZzJfNn/l6tXq8iWrNbo7JZDKZzAPMdrFDxr9eqnFMJpPJZL5rjmJ08QqkcEwmk8lkLpuT2vXk6z0mk8lkMs8wu4y5Wf5Fm8xkMplM5qIZGZNHilTtmEwmk8k82mzLW3pjVb+WYDKZTCbzTLPdEGZ3Yr/KZDKZTOaiWY5I59RC184xmUwmk3mAmWJU42pRa5OZTCaTyVw2jwomk8lkMplMJpPJZDKZTCaTyWQymUzm9e+a3yJS1HnqAYMGAAAAAElFTkSuQmCC" alt="Red dot" /> */}
       </div>
     </>
